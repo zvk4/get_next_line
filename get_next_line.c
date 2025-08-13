b66dbdb2de9c006b9fa65e9c118468e74403de8f
Zuleyha Karali <zkarali@k1m03s03.42istanbul.com.tr>
2025-08-13 15:54:23 +0300

    gnl son

--- get_next_line.c
+++ get_next_line.c
@@ -6,82 +6,82 @@
 /*   By: zkarali <zkarali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 14:59:27 by zkarali           #+#    #+#             */
-/*   Updated: 2025/08/07 19:01:45 by zkarali          ###   ########.fr       */
+/*   Updated: 2025/08/13 15:33:09 by zkarali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *extra_line(char **rest, int nl_i)
+static char	*extra_line(char **rest, int nl_i)
 {
-    char *line;
-    char *oldrest;
-    int len;
+	char	*line;
+	char	*oldrest;
+	int		len;
 
-    line = sub_str(*rest, 0, nl_i);
-    len = str_len(*rest) - (nl_i + 1);
-    oldrest = *rest;
-    *rest = sub_str(*rest, nl_i + 1, len);
-    free(oldrest);
-    return (line);
+	line = sub_str(*rest, 0, nl_i + 1);
+	len = str_len(*rest) - (nl_i + 1);
+	oldrest = *rest;
+	*rest = sub_str(*rest, nl_i + 1, len);
+	free(oldrest);
+	return (line);
 }
 
-static char *append_rest(char *rest, char *buf)
+static char	*append_rest(char *rest, char *buf)
 {
-    char *tmp;
+	char	*tmp;
 
-    tmp = rest;
-    rest = str_join(rest, buf);
-    free(tmp);
-    return (rest);
+	tmp = rest;
+	rest = str_join(rest, buf);
+	free(tmp);
+	return (rest);
 }
 
-static char *read_to_rest(int fd, char *rest)
+static char	*read_to_rest(int fd, char *rest)
 {
-    char *buf;
-    int bytes;
+	char	*buf;
+	ssize_t	bytes;
 
-    buf = malloc(BUF_SIZE + 1);
-    if (!buf)
-        return (NULL);
-    bytes = 1;
-    while (newline_i(rest) < 0 && bytes > 0)
-    {
-        bytes = read(fd, buf, BUF_SIZE);
-        if (bytes < 0)
-        {
-            free(buf);
-            return (NULL);
-        }
-        buf[bytes] = '\0';
-        rest = append_rest(rest, buf);
-    }
-    free(buf);
-    return (rest);
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	bytes = 1;
+	while (newline_i(rest) < 0 && bytes > 0)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[bytes] = '\0';
+		rest = append_rest(rest, buf);
+	}
+	free(buf);
+	return (rest);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *rest;
-    char *line;
-    int nl_i;
+	static char	*rest;
+	char		*line;
+	int			nl_i;
 
-    if (BUF_SIZE <= 0 || fd < 0)
-        return (NULL);
-    rest = read_to_rest(fd, rest);
-    if (!rest)
-        return (NULL);
-    nl_i = newline_i(rest);
-    if (nl_i >= 0)
-        return (extra_line(&rest, nl_i));
-    if (rest && *rest)
-    {
-        line = sub_str(rest, 0, str_len(rest));
-        free(rest);
-        rest = NULL;
-        return (line);
-    }
-    free(rest);
-    rest = NULL;
-    return (NULL);
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	rest = read_to_rest(fd, rest);
+	if (!rest)
+		return (NULL);
+	nl_i = newline_i(rest);
+	if (nl_i >= 0)
+		return (extra_line(&rest, nl_i));
+	if (rest && *rest)
+	{
+		line = sub_str(rest, 0, str_len(rest));
+		free(rest);
+		rest = NULL;
+		return (line);
+	}
+	free(rest);
+	rest = NULL;
+	return (NULL);
 }	
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,22 +6,22 @@
 /*   By: zkarali <zkarali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 15:00:27 by zkarali           #+#    #+#             */
-/*   Updated: 2025/08/07 19:07:14 by zkarali          ###   ########.fr       */
+/*   Updated: 2025/08/13 15:36:02 by zkarali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t str_len(const char *s)
+size_t	str_len(const char *s)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if (!s)
-        return (0);
-    while(s[i])
-        i++;
-    return (i);
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
 }
 
 char	*str_dup(const char *s)
@@ -29,8 +29,8 @@ char	*str_dup(const char *s)
 	int		i;
 	char	*copy;
 
-    if (!s)
-        return (NULL);
+	if (!s)
+		return (NULL);
 	i = 0;
 	while (s[i])
 		i++;
@@ -47,72 +47,72 @@ char	*str_dup(const char *s)
 	return (copy);
 }
 
-char *str_join(const char *s1, const char *s2)
+char	*str_join(const char *s1, const char *s2)
 {
-    char *str;
-    size_t i;
-    size_t j;
-    
-    if (!s1 && !s2)
-        return (NULL);
-    if (!s1)
-        return (str_dup(s2));
-    if (!s2)
-        return (str_dup(s1));
-    str = malloc(str_len(s1) + str_len(s2) + 1);
-    if (!str)
-        return (NULL);
-    i = -1;
-    while (s1[++i])
-        str[i] = s1[i];
-    j = -1;
-    while (s2[++j])
-        str[i + j] = s2[j];
-    str[i + j] = '\0';
-    return (str);
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (str_dup(s2));
+	if (!s2)
+		return (str_dup(s1));
+	str = malloc(str_len(s1) + str_len(s2) + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
+	return (str);
 }
 
-char *sub_str(char const *s, unsigned int start, size_t len)
+char	*sub_str(char const *s, unsigned int start, size_t len)
 {
-    size_t i;
-    size_t s_len;
-    char *str;
+	size_t	i;
+	size_t	s_len;
+	char	*str;
 
-    if (!s)
-        return (NULL);
-    s_len = str_len(s);
-    if (start >= s_len)
-    {
-        str = malloc(1);
-        if (!str)
-            return (NULL);
-        str[0] = '\0';
-        return(str);
-    }
-    if (len > s_len - start)
-        len = s_len - start;
-    str = malloc(len + 1);
-    if (!str)
-        return (NULL);
-    i = -1;
-    while (++i < len)
-        str[i] = s[start + i];
-    str[i] = '\0';
-    return (str);
+	if (!s)
+		return (NULL);
+	s_len = str_len(s);
+	if (start >= s_len)
+	{
+		str = malloc(1);
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	if (len > s_len - start)
+		len = s_len - start;
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		str[i] = s[start + i];
+	str[i] = '\0';
+	return (str);
 }
 
-int newline_i(const char *s)
+int	newline_i(const char *s)
 {
-    int i;
-    
-    i = 0;
-    if (!s)
-        return (-1);
-    while (s[i])
-    {
-        if (s[i] == '\n')
-            return (i);
-        i++;
-    }
-    return (-1);
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
 }
